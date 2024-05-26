7800814d9e193e7dc93bd0a0415d860a3381ee5e
Jack Li <me@jackli.dev>
2024-05-26 01:54:55 -0700

    fix: finally add favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
-	return (
-		<>
-			<title>@jckli</title>
-			<meta content="width=device-width, initial-scale=1" name="viewport" />
-			<link rel="icon" href="https://avatars.githubusercontent.com/u/39673993" sizes="any" />
-		</>
-	);
+    return (
+        <>
+            <title>@jckli</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <link rel="icon" href="https://avatars.githubusercontent.com/u/39673993" sizes="any" />
+        </>
+    );
 }
