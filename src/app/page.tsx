c6b0f7b165fecadb058f1d777d2e2cd48cc3c948
Jack Li <me@jackli.dev>
2024-09-06 01:42:07 -0700

    refactor: change up main site layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,45 +4,55 @@ import { Technologies } from "@/components/Technologies";
 import { TopSongs } from "@/components/TopSongs";
 
 export default function Home() {
-    return (
-        <>
-            <div className="flex-grow flex flex-col md:grid md:grid-cols-6 gap-6 font-metropolis">
-                <div className="col-span-3 sm:col-span-2 h-full">
-                    <CurrentTime />
-                </div>
-                <div className="col-span-3 sm:col-span-4 h-full">
-                    <div className="h-full rounded-xl p-6 border-[1px] border-borcol bg-box min-h-[13rem]">
-                        <h1 className="text-xl text-text-lighter font-metropolis-bold">Domains</h1>
-                        <p className="text-text-color mt-2">My only two domains, excluding their subdomains, are:</p>
-                        <ul className="list-disc list-inside text-text-color mt-2">
-                            <li>jackli.dev (for all personal things, like home services)</li>
-                            <li>hayasaka.moe (for all projects that I code)</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-span-4 h-full">
-                    <div className="h-full rounded-xl p-6 border-[1px] border-borcol bg-box min-h-[13rem]">
-                        <h1 className="text-xl text-text-lighter font-metropolis-bold">Who am I?</h1>
-                        <p className="text-text-color mt-2">
-                            I&apos;m currently a computer science major at Arizona State University. I like creating
-                            cool things in my free time using modern, efficient, and scalable technologies.
-                        </p>
-                        <p className="text-text-color mt-2">
-                            When I&apos;m not coding, you can catch me grabbing boba with friends, tinkering with my
-                            home server, or creating new random playlists.
-                        </p>
-                    </div>
-                </div>
-                <div className="col-span-2 h-full">
-                    <Status />
-                </div>
-                <div className="col-span-3 xl:col-span-2 h-full">
-                    <Technologies />
-                </div>
-                <div className="col-span-3 xl:col-span-4 h-full">
-                    <TopSongs />
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="flex-grow flex flex-col md:grid md:grid-cols-6 gap-6 font-metropolis">
+				<div className="col-span-3 sm:col-span-2 h-full">
+					<CurrentTime />
+				</div>
+				<div className="col-span-3 sm:col-span-4 h-full">
+					<div className="h-full rounded-xl p-6 border-[1px] border-borcol bg-box min-h-[13rem]">
+						<h1 className="text-xl text-text-lighter font-metropolis-bold">
+							Who am I?
+						</h1>
+						<p className="text-text-color mt-2">
+							I&apos;m currently a computer science major at Arizona State
+							University. I like creating cool things in my free time using
+							modern, efficient, and scalable technologies.
+						</p>
+						<p className="text-text-color mt-2">
+							When I&apos;m not coding, you can catch me grabbing boba with
+							friends, tinkering with my home server, or creating new random
+							playlists.
+						</p>
+					</div>
+				</div>
+				<div className="col-span-4 h-full">
+					<div className="h-full rounded-xl p-6 border-[1px] border-borcol bg-box min-h-[13rem]">
+						<h1 className="text-xl text-text-lighter font-metropolis-bold">
+							Domains
+						</h1>
+						<p className="text-text-color mt-2">
+							My only two domains, excluding their subdomains, are:
+						</p>
+						<ul className="list-disc list-inside text-text-color mt-2">
+							<li>
+								jackli.dev (for all personal things, like home services)
+							</li>
+							<li>hayasaka.moe (for all projects that I code)</li>
+						</ul>
+					</div>
+				</div>
+				<div className="col-span-2 h-full">
+					<Status />
+				</div>
+				<div className="col-span-3 xl:col-span-2 h-full">
+					<Technologies />
+				</div>
+				<div className="col-span-3 xl:col-span-4 h-full">
+					<TopSongs />
+				</div>
+			</div>
+		</>
+	);
 }
